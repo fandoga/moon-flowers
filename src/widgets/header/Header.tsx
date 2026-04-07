8ece12d73e7e22f943b08b414eb70f68ff4a4797
Trolozor <mplololowka@mail.ru>
2026-04-07 16:27:26 +0300

    ашч

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -128,7 +128,7 @@ const Header = () => {
                 </span>
               </div>
 
-              <div className="flex items-start space-x-1 mt-1 ml-0">
+              <div className="flex items-start space-x-1">
                 <svg
                   width="11"
                   height="15"
