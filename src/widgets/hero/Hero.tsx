0e52dc10e6de0379f98efe223871c420e5c87ca2
Artem / fandy <artem.nesterenko.1984@bk.ru>
2026-05-24 12:48:56 +0300

    change image loading

--- src/widgets/hero/Hero.tsx
+++ src/widgets/hero/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
         >
           <div className="h-[500px] w-full overflow-hidden rounded-xl md:rounded-2xl lg:rounded-[40px]">
             <Image
-              priority
+              loading="eager"
               src="/hero/image.png"
               className="w-full h-full object-cover"
               alt="bg_img"
